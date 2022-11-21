8ebcfde72671da21cd4a02a3e190d823ba43515b
Alb20 <alberto_sl@tesch.edu.mx>
2022-11-20 21:07:33 -0600

    Crear cosas con ngIf y ngFor

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -6,5 +6,13 @@ import { Component } from "@angular/core";
     templateUrl: `./body.component.html`
 })
 export class BodyComponent{
+    mostrar = true;
+
+    frase : any = {
+        mensaje : 'Un gran poder lleva una gran responsabilidad',
+        autor : 'Ben Parker'
+    };
+
+    personajes : string[]= ['Spiderman','Venom', 'Dr. Octopus']
 
 }
